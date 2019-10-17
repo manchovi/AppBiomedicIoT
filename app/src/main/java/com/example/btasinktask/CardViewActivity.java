affb50d5fe4c8d46d1ecc0eda3bfc8ba1956d023
Prof. Gamez L <winmanuel07@gmail.com>
2019-10-17 11:03:33 -0600

    Adicione un cardview bien personalizado

---
+++ app/src/main/java/com/example/btasinktask/CardViewActivity.java
@@ -0,0 +1,79 @@
+package com.example.btasinktask;
+
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CardViewActivity extends AppCompatActivity {
+
+    private ViewGroup linearLayoutDetails;
+    private ImageView imageViewExpand;
+
+    private static final int DURATION = 250;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardview);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);
+
+        linearLayoutDetails = (ViewGroup) findViewById(R.id.linearLayoutDetails);
+        imageViewExpand = (ImageView) findViewById(R.id.imageViewExpand);
+
+        Toolbar toolbarCard = (Toolbar) findViewById(R.id.toolbarCard);
+        toolbarCard.setTitle(R.string.olinguito);
+        toolbarCard.setSubtitle(R.string.subtitle);
+        toolbarCard.inflateMenu(R.menu.menu_card);
+        toolbarCard.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_option1:
+                        Toast.makeText(CardViewActivity.this, R.string.option1, Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_option2:
+                        Toast.makeText(CardViewActivity.this, R.string.option2, Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_option3:
+                        Toast.makeText(CardViewActivity.this, R.string.option3, Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    public void toggleDetails(View view) {
+        if (linearLayoutDetails.getVisibility() == View.GONE) {
+            ExpandAndCollapseViewUtil.expand(linearLayoutDetails, DURATION);
+            imageViewExpand.setImageResource(R.mipmap.more);
+            rotate(-180.0f);
+        } else {
+            ExpandAndCollapseViewUtil.collapse(linearLayoutDetails, DURATION);
+            imageViewExpand.setImageResource(R.mipmap.less);
+            rotate(180.0f);
+        }
+    }
+
+    private void rotate(float angle) {
+        Animation animation = new RotateAnimation(0.0f, angle, Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        animation.setFillAfter(true);
+        animation.setDuration(DURATION);
+        imageViewExpand.startAnimation(animation);
+    }
+
+
+}
--- app/src/main/java/com/example/btasinktask/MenuPrincipal.java
+++ app/src/main/java/com/example/btasinktask/MenuPrincipal.java
@@ -104,7 +104,6 @@ public class MenuPrincipal extends AppCompatActivity {
         carouselView.setImageListener(imageListener);
 
 
-
     }
 
 
