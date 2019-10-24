0994887e4c1ca5b7ae142fd1fddf578aef39520d
Prof. Gamez L <winmanuel07@gmail.com>
2019-10-24 15:39:40 -0600

    Todos los checkbox personalizados#

--- app/src/main/java/com/example/btasinktask/SignalsMonitor.java
+++ app/src/main/java/com/example/btasinktask/SignalsMonitor.java
@@ -991,8 +991,8 @@ public class SignalsMonitor extends AppCompatActivity implements MiAsyncTask.MiC
         //Activando leyendas:
         dataSeries.setTitle("SPO2");
         dataSeries.setDrawDataPoints(true);
-        dataSeries.setDataPointsRadius(3);
-        dataSeries.setThickness(2);
+        dataSeries.setDataPointsRadius(0);
+        dataSeries.setThickness(1);
 
         /*
         SetGraphParam(121, -0.5, 4.1, -4.1);
