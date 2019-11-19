7bf67b0672f0ed302e3834a2aa0093571329c705
Prof. Gamez L <winmanuel07@gmail.com>
2019-11-19 16:27:19 -0600

    Trabaje una tabla con llave foranea

--- app/src/main/java/com/example/btasinktask/db_SQLite.java
+++ app/src/main/java/com/example/btasinktask/db_SQLite.java
@@ -23,6 +23,10 @@ public class db_SQLite extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         //db.execSQL("create table usuarios(codigo integer not null primary key autoincrement,nombres varchar(50) not null,apellidos varchar(50) not null,usuario varchar(100) not null,clave varchar(10) not null,pregunta varchar(100) not null,respuesta varchar(100) not null, fecha datetime NOT NULL)");
         db.execSQL("create table tb_especialista(documento varchar(100) not null primary key,nombres varchar(50) not null,apellidos varchar(50) not null,direccion varchar(150),telefono varchar(15) not null,especialidad varchar(100),sexo varchar(15) not null, comentario varchar(250) not null, usuario varchar(100) not null, clave varchar(50) not null, pregunta varchar(100) not null,respuesta varchar(100) not null, fecha datetime NOT NULL)");
+
+        db.execSQL("create table tb_pacientes(codigo integer not null primary key autoincrement, dui varchar(15) not null, nombres varchar(50) not null, apellidos varchar(50) not null, direccion varchar(200) not null, telefono varchar(15) not null, estado varchar(255) not null, fecha datetime NOT NULL, comentario text, nombre_contacto_pariente varchar(250), telefono_contacto_pariente varchar(15), direccion_contacto_pariente varchar(250), documento_especialista varchar(15) not null, FOREIGN KEY(documento_especialista) REFERENCES tb_especialista(documento))");
+
+
         //db.execSQL("insert into usuarios values(null,'Administrador','admin','admin@utla.edu.sv','admin','¿Cuál es el nombre de tu universidad favorita?','UTLA',datetime())");
         db.execSQL("insert into tb_especialista values('1', 'Manuel de Jesús', 'Gámez López', 'Cas, San Nicolas Lempa, Tecoluca-San Vicente', '+50361107065', 'Docente Investigador', 'Masculino', 'N.A', 'manuel.gamez@itca.edu.sv', '12345', '¿Cuál es el nombre de tu universidad favorita?', 'MEGATEC-ZACATECOLUCA', datetime('now','localtime'))");
     }
@@ -30,6 +34,7 @@ public class db_SQLite extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists tb_especialista");
+        db.execSQL("drop table if exists tb_pacientes");
         onCreate(db);
     }
 
